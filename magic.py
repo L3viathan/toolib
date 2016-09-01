94ff793ff55f148a22725079f6aacc2c943abddf
L3viathan <git@l3vi.de>
2016-09-01 16:18:00 +0200

    repr() will now return minimally paranthesized strings.
    This means that every operation is annotated with its precedence value.
    Precendence values are the line number of the operation in the table on
    https://docs.python.org/3/reference/expressions.html#operator-precedence
    (one-indexed).
    
    Also cleaned up some docstrings.

--- magic.py
+++ magic.py
@@ -9,44 +9,59 @@ class Magic(object):
         self.is_original=is_original
         self.__ops = []
         self.__repr = 'ƒ'
+        self.__last_prec = 18
 
     def __repr__(self):
         return self.__repr
 
     def __call__(self, something):
-        ''' Maybe eventually extend to act like methodcaller, but problems exist'''
+        ''' Resolve the list of operations and return the final object. '''
+        # Maybe eventually extend to act like methodcaller, but problems exist
         if self.is_original:
-            # return Magic(is_original=False)
-            raise RuntimeError("Can't call getter directly")
+            raise RuntimeError("Can't call ƒ directly")
         for function in self.__ops:
             something = function(something)
         return something
 
-    def _add_function(self, function, left='', right=''):
+    def _add_function(self, function, left='', right='', prec=0):
+        '''
+        Adds a function on the operation list.
+
+        Additional arguments specify what strings should be added to the left
+        or right side, and the operator precedence value of the operation.
+        Parentheses are only added to the string representation if the new
+        precedence value is higher than the previous one.
+
+        If this is the one and only original ƒ object, make a non-original copy
+        and call _add_function on it with the same parameters.
+        '''
         if self.is_original:
             return type(self)(is_original=False)._add_function(
                     function,
                     left=left,
                     right=right,
+                    prec=prec,
                     )
-        if self.__ops:
+        if prec > self.__last_prec:
             self.__repr = '{left}({previous}){right}'.format(
                     left=left,
                     right=right,
                     previous=self.__repr,
                     )
+            self.__last_prec = prec
         else:
             self.__repr = '{left}{previous}{right}'.format(
                     left=left,
                     right=right,
                     previous=self.__repr,
                     )
+            self.__last_prec = prec
         self.__ops.append(function)
         return self
 
     def __getattr__(self, attr):
         '''
-        getter.foo
+        ƒ.foo
 
         If you want to use this with double underscore attributes, add a third
         underscore, e.g. ƒ.___name__
@@ -55,57 +70,59 @@ class Magic(object):
             attr = attr[1:]
         elif attr.startswith('__'):
            raise AttributeError(item)
-        return self._add_function(op.attrgetter(attr), right='.'+attr)
+        return self._add_function(op.attrgetter(attr), right='.'+attr, prec=16)
 
     def __getitem__(self, item):
-        '''getter[foo]'''
-        return self._add_function(op.itemgetter(item), right='['+repr(item)+']')
+        '''ƒ[foo]'''
+        return self._add_function(op.itemgetter(item), right='['+repr(item)+']', prec=16)
 
     def __matmul__(self, other):
-        '''Not actually matmul. getter@function. Applies function.'''
-        return self._add_function(other, right='@'+other.__name__)
+        '''Not actually matmul. ƒ@other. Applies function.'''
+        return self._add_function(other, right='@'+other.__name__, prec=12)
 
     def __eq__(self, other):
-        return self._add_function(partial(op.eq, other), right='=='+repr(other))
+        '''ƒ == other'''
+        return self._add_function(partial(op.eq, other), right='=='+repr(other), prec=7)
 
     def __add__(self, other):
-        return self._add_function(partial(op.add, other), right='+'+repr(other))
+        '''ƒ + other'''
+        return self._add_function(partial(op.add, other), right='+'+repr(other), prec=11)
 
     def __and__(self, other):
         ''' ƒ & foo '''
-        return self._add_function(partial(op.and_, other), right='&'+repr(other))
+        return self._add_function(partial(op.and_, other), right='&'+repr(other), prec=9)
 
     def __mod__(self, other):
         ''' ƒ % foo '''
-        return self._add_function(lambda x: x%other, right='%'+repr(other))
+        return self._add_function(lambda x: x%other, right='%'+repr(other), prec=12)
 
     def __mul__(self, other):
-        ''' ƒ % foo '''
-        return self._add_function(partial(op.mul, other), right='*'+repr(other))
+        ''' ƒ * foo '''
+        return self._add_function(partial(op.mul, other), right='*'+repr(other), prec=12)
 
     def __neg__(self):
         ''' -ƒ '''
-        return self._add_function(partial(op.neg, other), left='-')
+        return self._add_function(partial(op.neg, other), left='-', prec=13)
 
     def __not__(self):
         ''' not ƒ'''
-        return self._add_function(partial(op.not_, other), left='not ')
+        return self._add_function(partial(op.not_, other), left='not ', prec=6)
 
     def __or__(self, other):
         ''' ƒ | foo '''
-        return self._add_function(partial(op.or_, other), right='|'+repr(other))
+        return self._add_function(partial(op.or_, other), right='|'+repr(other), prec=8)
 
     def __pow__(self, other):
         ''' ƒ ** foo '''
-        return self._add_function(lambda x: x**other, right='**'+repr(other))
+        return self._add_function(lambda x: x**other, right='**'+repr(other), prec=14)
 
     def __sub__(self, other):
         ''' ƒ - foo '''
-        return self._add_function(lambda x: x-other, right='-'+repr(other))
+        return self._add_function(lambda x: x-other, right='-'+repr(other), prec=11)
 
     def __xor__(self, other):
         ''' ƒ ^ foo '''
-        return self._add_function(partial(op.xor, other), right='-'+repr(other))
+        return self._add_function(partial(op.xor, other), right='-'+repr(other), prec=8)
 
 
 ƒ = Magic()
