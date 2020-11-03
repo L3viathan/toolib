27e1af29b894ad68c73903afb35ceabda72edebf
L3viathan <git@l3vi.de>
2020-11-03 20:24:21 +0100

    Don't execute on import; make less chatty

--- retry.py
+++ retry.py
@@ -8,26 +8,23 @@ def retry(tries):
             ex = None
             for i in range(tries):
                 try:
-                    print("Try nr.", i)
                     val = fn(*args, **kwargs)
-                    print("Success!")
                 except Exception as e:
-                    print("Failed")
                     ex = e
                 else:
                     return val
             if ex is not None:
-                print("Max. tries exceeded, reraising")
                 raise ex
         return wrapper
     return decorator
 
 
-@retry(3)
-def fail_often():
-    if random.random() > 0.9:
-        return "yay"
-    raise RuntimeError
+if __name__ == '__main__':
+    @retry(3)
+    def fail_often():
+        if random.random() > 0.9:
+            return "yay"
+        raise RuntimeError
 
 
-fail_often()
+    fail_often()
